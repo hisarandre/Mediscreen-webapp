9cb51018dfa46235451094ade7f0779f0986fd09
Sara Andre <hisarandre@gmail.com>
2023-07-29 22:20:58 +0200

    Add Error decoder

---
+++ src/main/java/com/mediscreen/webapp/exception/CustomErrorDecoder.java
@@ -0,0 +1,33 @@
+package com.mediscreen.webapp.exception;
+
+import feign.Response;
+import feign.codec.ErrorDecoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
+@Component
+public class CustomErrorDecoder implements ErrorDecoder {
+
+    private final ErrorDecoder defaultErrorDecoder = new Default();
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public Exception decode(String methodKey, Response response) {
+
+
+        switch (response.status()){
+            case 400:
+                logger.error("Status code " + response.status() + ", methodKey = " + methodKey);
+            case 404:
+            {
+                logger.error("Error took place when using Feign client to send HTTP Request. Status code " + response.status() + ", methodKey = " + methodKey);
+                return new ResponseStatusException(HttpStatus.valueOf(response.status()), "<You can add error message description here>");
+            }
+            default:
+                return new Exception(response.reason());
+        }
+    }
+}
